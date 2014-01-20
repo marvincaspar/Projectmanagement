f3b47049bfa1fbf93f3283f74764247adbd72108
marvin <marvin.caspar@gmail.com>
2014-01-20 18:04:41 +0100

    roadmap

--- app/assets/javascripts/milestones.js
+++ app/assets/javascripts/milestones.js
@@ -8,4 +8,25 @@ function initMilestone() {
     var id = jQuery(this).parents('.dd-item').data('id');
     jQuery('#milestone_form').show();
   });
+
+  jQuery('#milestone .glyphicon-pencil').click(function() {
+    alert('MUSS NOCH GEMACHT WERDEN!!!');
+    return false;
+
+    var id = jQuery(this).parents('.dd-item').data('id');
+    var show_url = pbs_edit_url.replace(':id', id);
+
+    jQuery.ajax({
+      type: 'GET',
+      url: show_url,
+      dataType : 'json'
+    }).done(function ( data ) {
+      jQuery('#pbs_show').hide();
+      fillForm(data, jQuery('#pbs_form'));
+      // jQuery('#work_package_id').val(id);
+      jQuery('#pbs_form').show();
+    }).fail(function ( data ) {
+      alert('error');
+    });
+  });
 }
